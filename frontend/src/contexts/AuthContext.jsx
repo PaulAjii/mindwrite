766fe25dd113da7e8c140b230097cadd216abd20
PaulAjii <paulajii134@gmail.com>
2023-04-13 03:12:44 +0100

    Added useFetch hook

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useEffect } from "react";
+import { createContext, useReducer, useEffect } from "react"
 
 export const AuthContext = createContext()
 
---
+++ frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const useFetch = (url) => {
+  const [ data, setData ] = useState(null)
+  const [ isLoading, setIsLoading ] = useState(true)
+  const [ error, setError ] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setData(data)
+        setIsLoading(false)
+      } catch (error) {
+        setError(error)
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [url])
+
+  return { data, isLoading, error }
+}
+
+export default useFetch
