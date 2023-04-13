6765e6c06f28f5a89878b1b654abedc3e7399e9b
PaulAjii <paulajii134@gmail.com>
2023-04-13 03:19:03 +0100

    Made a slight change to the Navigation bar

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 const NavigationBar = ({ handleToggle }) => {
+  const { user } = useAuthContext()
+
   return (
     <header>
       <div className="container">
@@ -9,13 +12,26 @@ const NavigationBar = ({ handleToggle }) => {
         </Link>
 
         <nav className="nav">
-          <div className="nav-links">
-            <Link to="/about-us" className="nav-link">know us</Link>
-            <Link to="/posts" className="nav-link">read stories</Link>
-            <Link to="/login" className="nav-link">sign in</Link>
-            <Link to="/contact-us" className="nav-link">reach us</Link>
-            <Link to="/signup" className="nav-link">get started</Link>
-          </div>
+          {
+            !user 
+              ?
+              <div className="nav-links">
+              <Link to="/about-us" className="nav-link">know us</Link>
+              <Link to="/posts" className="nav-link">read stories</Link>
+              <Link to="/login" className="nav-link">sign in</Link>
+              <Link to="/contact-us" className="nav-link">reach us</Link>
+              <Link to="/signup" className="nav-link">get started</Link>
+            </div>
+              :
+              <div className="nav-links">
+              <Link to="/about-us" className="nav-link">know us</Link>
+              <Link to="/posts" className="nav-link">read stories</Link>
+              <Link to="/contact-us" className="nav-link">reach us</Link>
+              <Link to="/login" className="nav-link">sign out</Link>
+              <Link to="/signup" className="nav-link">dashboard</Link>
+            </div>
+          }
+          
           <div className="toggle-mode">
             <button className='btn' onClick={ handleToggle }>toggle</button>
           </div>
